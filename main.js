06a4c6b307c6fa499a965b2be43bba687c56d4c0
SithLor <84148006+SithLor@users.noreply.github.com>
2022-10-19 07:46:06 -0700

    h

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+import { math } from './Moduels/math/math'
+import { log } from './Moduels/log/logger'
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello, world!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+logger.log("FCK")
